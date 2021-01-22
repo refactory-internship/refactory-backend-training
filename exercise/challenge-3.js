149f3ddc0474fb9c2b4c2441b2d2e168a2ccf67f
Fajar Pratama <pfajarfa14.9b@gmail.com>
2021-01-22 11:48:20 +0700

    finished challenge-3.js

--- exercise/challenge-3.js
+++ exercise/challenge-3.js
@@ -1,12 +1,43 @@
-function calculate() {
-  const array = [-6, -4, 0, 1, 2, 2, 3, 10, 12, 44, 52, 72, 98];
+function calculate(length) {
+  const array = Array(length).fill().map(() => Math.round(Math.random() * length));
+  const len = array.length;
+  const arraySort = array.sort();
+  const mid = Math.ceil(len / 2);
 
+  //find sum
   let sum = 0;
-  for (let index = 0; index < array.length; index++) {
-    sum += array[index];
+  for (let index = 0; index < len; index++) {
+    sum += arraySort[index];
   }
 
-  console.log(sum);
+  //find minimum value
+  let min = arraySort[0];
+  for (let index = 0; index < len; index++) {
+    if (arraySort[index] < min) {
+      min = arraySort[index];
+    }
+  }
+
+  //find maximum value
+  let max = arraySort[0];
+  for (let index = 0; index < len; index++) {
+    if (arraySort[index] > max) {
+      max = arraySort[index];
+    }
+  }
+
+  //find average
+  const average = (sum / len).toFixed(2);
+
+  //find median
+  const median = len % 2 == 0 ? (arraySort[mid] + arraySort[mid - 1]) / 2 : arraySort[mid - 1];
+
+  console.log('Your numbers = ' + arraySort);
+  console.log('Maximum value =  ' + max);
+  console.log('Minimum value =  ' + min);
+  console.log('Median =  ' + median);
+  console.log('Average =  ' + average);
+  // console.log('Sum =  ' + sum);
 }
 
-calculate();
+calculate(7);
